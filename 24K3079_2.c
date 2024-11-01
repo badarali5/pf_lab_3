2c8af10dfe00492eb69b34a1966ee2b6ddd91c31
badarali5 <badaralinaqvi512@gmail.com>
2024-11-01 15:58:14 +0500

    Add files via upload

---
+++ 24K3079_2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char slogans[10][100];
+    int slogansNum;
+    printf("Enter the number of slogans: ");
+    scanf("%d",&slogansNum);
+    getchar();
+    int i;
+    for(i=0;i<slogansNum;i++){
+        printf("Enter slogan %d: ",i+1);
+        fgets(slogans[i],100,stdin);
+        int l=strlen(slogans[i]);
+        if(l>0 && slogans[i][l-1]=='\n'){
+            slogans[i][l-1]='\0';
+        }
+    }
+    int ASCIIvalue=128;
+    int frequency[ASCIIvalue];
+    int m, j;
+    for(i=0; i<slogansNum; i++){
+        for(m=0; m<ASCIIvalue; m++){
+            frequency[m] = 0;
+        }
+        for (j=0; slogans[i][j]!='\0'; j++){
+            frequency[slogans[i][j]]++;
+        }
+        printf("Frequency for \"%s\": {", slogans[i]);
+        for (j=0; j<ASCIIvalue;j++){
+            if (frequency[j]>0){
+                printf("'%c': %d",j,frequency[j]);
+                if (j<ASCIIvalue-1){
+                    printf(", ");
+                }
+            }
+        }
+        printf("}\n");
+    }
+    return 0;
+}
---
+++ 24K3079_4.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+void sortTransaction(char sorted[]) {
+    int l=strlen(sorted);
+    int i, j;
+    for (i=0; i<l-1; i++) {
+        for (j=i; j<l; j++) {
+            if (sorted[i]>sorted[j]) {
+                char temp=sorted[i];
+                sorted[i]=sorted[j];
+                sorted[j]=temp;
+            }
+        }
+    }
+}
+void groupTransactions(char transactions[][100],int size) {
+    int grouped[100]={0};
+    char sortedTransaction[100][100];
+    int i;
+    for (i=0; i<size; i++) {
+        strcpy(sortedTransaction[i],transactions[i]);
+        sortTransaction(sortedTransaction[i]);
+    }
+    int k, j;
+    for (i=0; k<size; k++) {
+        if(!grouped[i]) {
+            printf("Group: %s",transactions[i]);
+            grouped[i]=1;
+            for (j=k+1; j<size; j++) {
+                if (strcmp(sortedTransaction[i],sortedTransaction[j])==0) {
+                    printf(", %s",transactions[j]);
+                    grouped[j]=1;
+                }
+            }
+            printf("\n");
+        }
+    }
+}
+int main() {
+    char transactions[10][100];
+    int transactionsNum;
+    printf("Enter the number of transactions: ");
+    scanf("%d",&transactionsNum);
+    getchar();
+    printf("Input transactions\n[ ");
+    int i;
+    int j;
+    for (i=0; i<transactionsNum; i++) {
+        if (i==transactionsNum-1) {
+            printf("Transaction %d: ",i+1);
+            fgets(transactions[i], 100, stdin);
+            transactions[i][strcspn(transactions[i],"\n")]=0;
+            printf("]\n");
+            break;    
+        }
+        printf("Transaction %d: ",i+1);
+        fgets(transactions[i],100,stdin);
+        for (j=0; transactions[i][j]!='\0'; j++) {
+        	 if (transactions[i][j]=='\n') {
+        	 	 transactions[i][j]='\0';
+                 break;
+        }
+    }
+    }
+    groupTransactions(transactions,transactionsNum);
+    return 0;
+}
